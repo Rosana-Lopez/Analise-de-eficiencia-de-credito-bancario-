696079a6a86fb95bb3f2db37922c67ae2cfd205a
Rosana Lopez <rosanacamposlopez@gmail.com>
2026-04-23 08:33:42 -0300

    Adiciona script de verificacao dos dados no banco

---
+++ sql/verificacao.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*) AS total_clientes FROM clientes;
+SELECT COUNT(*) AS total_analistas FROM analistas;
+SELECT COUNT(*) AS total_solicitacoes FROM solicitacoes_credito;
+SELECT COUNT(*) AS total_decisoes FROM decisoes;
