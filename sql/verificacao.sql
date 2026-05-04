df0f50211cd383b972db89e0a27e6a4dfcef54fc
Rosana Lopez <rosanacamposlopez@gmail.com>
2026-05-04 08:33:44 -0300

    Adiciona modelo de ML com Random Forest e balanceamento de classes

--- sql/verificacao.sql
+++ sql/verificacao.sql
@@ -2,3 +2,14 @@ SELECT COUNT(*) AS total_clientes FROM clientes;
 SELECT COUNT(*) AS total_analistas FROM analistas;
 SELECT COUNT(*) AS total_solicitacoes FROM solicitacoes_credito;
 SELECT COUNT(*) AS total_decisoes FROM decisoes;
+SELECT MIN(id_analista), MAX(id_analista) FROM analistas;
+SELECT MIN(id_cliente), MAX(id_cliente) FROM clientes;
+DELETE FROM decisoes;
+DELETE FROM solicitacoes_credito;
+DELETE FROM analistas;
+DELETE FROM clientes;
+
+DBCC CHECKIDENT ('decisoes', RESEED, 0);
+DBCC CHECKIDENT ('solicitacoes_credito', RESEED, 0);
+DBCC CHECKIDENT ('analistas', RESEED, 0);
+DBCC CHECKIDENT ('clientes', RESEED, 0);
